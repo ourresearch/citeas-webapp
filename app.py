8c14a5c6efd4676594cd4d8215df5cdc66757a66
Casey M <caseym@gmail.com>
2021-04-23 06:08:41 -0500

    add sentry monitoring

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import os
 from flask import Flask
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
+# error reporting with sentry
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_DSN'),
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)
-# app.debug = True
